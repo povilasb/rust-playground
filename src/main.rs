d3b0e6f5e9fa96dde3b0d4a8293defa9bdeda427
Povilas Balciunas <balciunas90@gmail.com>
2017-09-07 22:27:20 +0300

    Implement stdio input

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #[cfg(test)] #[macro_use] extern crate hamcrest;
 
+use std::io;
+
 mod tree;
 use tree::Tree;
 
@@ -9,4 +11,39 @@ fn main() {
     t.append(Some(4), None, 2);
     t.append(Some(5), None, 3);
     println!("{}", t.to_string());
+
+    let indexes = read_indexes();
+    for n in indexes {
+        println!("{:?}", n);
+    }
+}
+
+fn read_indexes() -> Vec<Option<u64>> {
+    read_numbers().iter()
+        .map(|n: &i64| {
+            if *n < 0i64 {
+                None
+            }
+            else {
+                Some(*n as u64)
+            }
+        })
+        .collect()
+}
+
+fn read_numbers() -> Vec<i64> {
+    match readln() {
+        Ok(ln) => {
+            ln.split_whitespace()
+                .map(|s| s.parse::<i64>().unwrap())
+                .collect()
+        },
+        Err(_) => Vec::new(),
+    }
+}
+
+fn readln() -> io::Result<String> {
+    let mut ln = String::new();
+    io::stdin().read_line(&mut ln)?;
+    Ok(String::from(ln.trim()))
 }
