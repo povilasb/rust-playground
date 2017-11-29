ddbb9086444592b2ebdbc514335ae6f905f9fd9f
Povilas Balciunas <balciunas90@gmail.com>
2017-11-29 13:22:58 +0200

    Implement simple tcp connection handler

--- tokio-client-server/src/main.rs
+++ tokio-client-server/src/main.rs
@@ -4,12 +4,32 @@ extern crate tokio_core;
 extern crate futures;
 use futures::future;
 
+use std::net::SocketAddr;
+use std::{thread, time};
+
 use tokio_core::reactor::Core;
+use tokio_core::net::{TcpListener, TcpStream};
+use futures::Stream;
 
 fn main() {
     let mut core = unwrap!(Core::new());
-    let res: Result<(), ()> = core.run(future::lazy(|| {
-        println!("{}", "running inside event loop!");
+
+    let listener = unwrap!(TcpListener::bind(
+        &unwrap!("0.0.0.0:5000".parse()),
+        &core.handle(),
+    ));
+    let accept_conns = listener.incoming().for_each(|(client, addr)| {
+        core.handle().spawn_fn(|| {
+            on_connected(client, addr);
+            Ok(())
+        });
         Ok(())
-    }));
+    });
+
+    unwrap!(core.run(accept_conns));
+}
+
+fn on_connected(client: TcpStream, addr: SocketAddr) {
+    println!("Connected: {}", addr);
+    thread::sleep(time::Duration::from_secs(5));
 }
