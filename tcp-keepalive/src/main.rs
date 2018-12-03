ca9433f894483a1aa12e8941e797da412d8544cc
Povilas Balciunas <balciunas90@gmail.com>
2018-12-03 17:43:34 +0200

    Separate linux specific code

--- tcp-keepalive/src/main.rs
+++ tcp-keepalive/src/main.rs
@@ -7,9 +7,12 @@ extern crate unwrap;
 
 use mio::net::TcpStream;
 use std::io;
-use std::mem;
-use std::os::unix::io::{AsRawFd, RawFd};
 use std::time::Duration;
+
+#[cfg(not(target_os = "linux"))]
+use other::set_keep_alive;
+#[cfg(target_os = "linux")]
+use unix::set_keep_alive;
 
 fn main() {
     let stream = unwrap!(TcpStream::connect(&addr!("192.168.1.210:6000")));
@@ -18,30 +21,57 @@ fn main() {
     let _ = unwrap!(readln());
 }
 
-/// Set TCP keep alive options for a given socket.
-fn set_keep_alive(stream: &TcpStream, idle: u32, interval: u32, count: u32) -> io::Result<()> {
-    stream.set_keepalive(Some(Duration::from_secs(u64::from(idle))))?;
-    let fd = stream.as_raw_fd();
-    set_ip_opt(fd, libc::TCP_KEEPINTVL, interval)?;
-    set_ip_opt(fd, libc::TCP_KEEPCNT, count)?;
-    Ok(())
+#[cfg(not(target_os = "linux"))]
+mod other {
+    use super::*;
+
+    /// Do nothing on unknown systems.
+    pub fn set_keep_alive(
+        _stream: &TcpStream,
+        _idle: u32,
+        _interval: u32,
+        _count: u32,
+    ) -> io::Result<()> {
+        Ok(())
+    }
 }
 
-/// Sets IP level option for a given socketlevel option for a given socket
-fn set_ip_opt(sock_fd: RawFd, opt: libc::c_int, val: u32) -> io::Result<()> {
-    unsafe {
-        let optval: libc::c_int = val as libc::c_int;
-        let ret = libc::setsockopt(
-            sock_fd,
-            libc::IPPROTO_TCP,
-            opt,
-            &optval as *const _ as *const libc::c_void,
-            mem::size_of_val(&optval) as libc::socklen_t,
-        );
-        if ret != 0 {
-            Err(io::Error::last_os_error())
-        } else {
-            Ok(())
+#[cfg(target_os = "linux")]
+mod unix {
+    use super::*;
+    use std::mem;
+    use std::os::unix::io::{AsRawFd, RawFd};
+
+    /// Set TCP keep alive options for a given socket.
+    pub fn set_keep_alive(
+        stream: &TcpStream,
+        idle: u32,
+        interval: u32,
+        count: u32,
+    ) -> io::Result<()> {
+        stream.set_keepalive(Some(Duration::from_secs(u64::from(idle))))?;
+        let fd = stream.as_raw_fd();
+        set_ip_opt(fd, libc::TCP_KEEPINTVL, interval)?;
+        set_ip_opt(fd, libc::TCP_KEEPCNT, count)?;
+        Ok(())
+    }
+
+    /// Sets IP level option for a given socketlevel option for a given socket
+    fn set_ip_opt(sock_fd: RawFd, opt: libc::c_int, val: u32) -> io::Result<()> {
+        unsafe {
+            let optval: libc::c_int = val as libc::c_int;
+            let ret = libc::setsockopt(
+                sock_fd,
+                libc::IPPROTO_TCP,
+                opt,
+                &optval as *const _ as *const libc::c_void,
+                mem::size_of_val(&optval) as libc::socklen_t,
+            );
+            if ret != 0 {
+                Err(io::Error::last_os_error())
+            } else {
+                Ok(())
+            }
         }
     }
 }
